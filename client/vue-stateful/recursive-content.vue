4a14cc27eef021ce35c697d671fea2d5ddff515b
Mitar <mitar.git@tnode.com>
2017-07-18 04:08:12 -0700

    Use keys.

--- client/vue-stateful/recursive-content.vue
+++ client/vue-stateful/recursive-content.vue
@@ -3,11 +3,11 @@
     <li>
       <p>Depth {{depth}}</p>
       <template v-if="depth">
-        <recursive-content :depth="nextDepth" />
-        <recursive-content :depth="nextDepth" />
-        <recursive-content :depth="nextDepth" />
-        <recursive-content :depth="nextDepth" />
-        <recursive-content :depth="nextDepth" />
+        <recursive-content :depth="nextDepth" :key="1" />
+        <recursive-content :depth="nextDepth" :key="2" />
+        <recursive-content :depth="nextDepth" :key="3" />
+        <recursive-content :depth="nextDepth" :key="4" />
+        <recursive-content :depth="nextDepth" :key="5" />
       </template>
       <template v-else>
         <recursive-final />
